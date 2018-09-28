58ff810a9b17197b65245785845dfb177a24bb00
Elvis B. Martins <elvisbmartins@gmail.com>
2018-09-28 17:50:01 -0300

    singles base

---
+++ single-ofertas.php
@@ -0,0 +1,11 @@
+preco_pessoa_titulo
+preco
+preco_infos
+
+tempo_utilizacao_titulo
+tempo
+tempo_infos
+
+desconto_titulo
+desconto
+desconto_infos
---
+++ single-parcerias.php
@@ -0,0 +1,11 @@
+validade_titulo
+validade
+validade_infos
+
+utilizacao_titulo
+utilizacao
+utilizacao_infos
+
+desconto_titulo
+desconto
+desconto_titulo
