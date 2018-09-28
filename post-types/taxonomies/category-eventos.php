a6d2ece1c9176a137ee85eb25bd75ffc0d19c2b2
Elvis B. Martins <elvisbmartins@gmail.com>
2018-09-28 17:50:32 -0300

    taxonomies categoorys for post-types separadas

---
+++ post-types/taxonomies/category-eventos.php
@@ -0,0 +1,31 @@
+<?php
+// hook into the init action and call create_book_taxonomies when it fires
+add_action( 'init', 'category_eventos_taxonomies', 1 );
+
+function category_eventos_taxonomies() {
+	// Add new taxonomy, make it hierarchical (like categories)
+	$labels = array(
+		'name'              => _x( 'Categorias', 'taxonomy general name', 'textdomain' ),
+		'singular_name'     => _x( 'Categorias', 'taxonomy singular name', 'textdomain' ),
+		'search_items'      => __( 'Buscar Categorias', 'textdomain' ),
+		'all_items'         => __( 'Todos Categorias', 'textdomain' ),
+		'parent_item'       => __( 'Parent Categorias', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Categorias:', 'textdomain' ),
+		'edit_item'         => __( 'Editar Categoriass', 'textdomain' ),
+		'update_item'       => __( 'Atualizar Categoriass', 'textdomain' ),
+		'add_new_item'      => __( 'Add Novo Categoriass', 'textdomain' ),
+		'new_item_name'     => __( 'Novo Categoriass Nome', 'textdomain' ),
+		'menu_name'         => __( 'Categorias', 'textdomain' ),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'category_eventos' ),
+	);
+
+	register_taxonomy( 'category_eventos', array( 'eventos' ), $args );
+}
